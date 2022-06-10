9bf8a01c638f049c8aeac75d53e8b4cb3cdc475c
amorales <desarrollador@duramas.com.ec>
2022-06-10 10:46:01 -0500

    Archivos Inciales

---
+++ src/test/java/com/amorales/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amorales.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
