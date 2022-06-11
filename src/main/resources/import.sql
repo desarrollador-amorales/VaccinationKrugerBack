be5c4a53d4f4cb3993e9708386613a276cf338cc
amorales <desarrollador@duramas.com.ec>
2022-06-10 23:54:07 -0500

    Modificacion de version de Spring Boot para pruebas, claves para autenticacion de usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,6 +7,6 @@ INSERT INTO vaccination_type (id, description, name, status) VALUES('f7bf1d24-e9
 INSERT INTO vaccination_type (id, description, name, status) VALUES('fe9c98ce-e920-11ec-8fea-0242ac120002', 'JHONSON&JHONSON', 'JHONSON&JHONSON', true);
 
 INSERT INTO employee (id, created_by, created_on, updated_by, updated_on, birth_date, email, identification, names, second_surname, status, surname, vaccination_status) VALUES('1e459900-e921-11ec-8fea-0242ac120002', '', '2000-01-01 00:00:00.000', NULL, '2000-01-01 00:00:00.000', '2000-01-01', 'armj@gmail.com', '0930007539', 'ADMIN', '', true, 'ADMIN', 'Y');
-INSERT INTO auth_employee (id, created_by, created_on, updated_by, updated_on, id_employee, "password", status, username) VALUES('1e459900-e921-11ec-8fea-0242ac120002', '', '2022-01-01 00:00:00.000', '', '2022-01-01 00:00:00.000', 'cdcb652b-43d2-40f1-9cd6-a7469b0f3903', '$2a$10$6/Tt/iJQHnGslc86uYdWo.KlqHWWDMoCG88iyzk41xOd.tPZNa456', true, '0930007539');
+INSERT INTO auth_employee (id, created_by, created_on, updated_by, updated_on, id_employee, "password", status, username) VALUES('1e459900-e921-11ec-8fea-0242ac120002', '', '2022-01-01 00:00:00.000', '', '2022-01-01 00:00:00.000', '1e459900-e921-11ec-8fea-0242ac120002', '$2a$10$6/Tt/iJQHnGslc86uYdWo.KlqHWWDMoCG88iyzk41xOd.tPZNa456', true, '0930007539');
 
-INSERT INTO auth_role (auth_id, rol_id) VALUES('1e459900-e921-11ec-8fea-0242ac120002', '1e459900-e921-11ec-8fea-0242ac120002');
+INSERT INTO auth_role (auth_id, rol_id) VALUES('1e459900-e921-11ec-8fea-0242ac120002', '27396f02-e915-11ec-8fea-0242ac120002');
